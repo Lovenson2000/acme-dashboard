2cb034010310524b2263b48eb8a1bffa344b7125
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-01-24 23:24:33 +0800

    Handling Data mutations and Errors

---
+++ .history/app/lib/actions_20240124221006.ts
@@ -0,0 +1,15 @@
+'use server';
+import { z } from 'zod';
+
+const FormSchema = z.object()
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+    // const rawFormData = Object.fromEntries(formData.entries())
+    console.log(rawFormData);
+    console.log(typeof rawFormData.amount);
+}
